354c4b9a9ebf87d300e75f6cf958c413d007b251
rafique-devops <thedevops91@gmail.com>
2024-02-10 12:44:07 +0530

    added increment operator

--- back_to_basics.js
+++ back_to_basics.js
@@ -10,18 +10,27 @@ let age = 31
 let isActive = true
 let salary = 10000000000
 
-console.log("name: ",name)
-console.log("age: ", age)
-console.log("status: ",isActive);
-console.log("salary: ",salary);
+// console.log("name: ",name)
+// console.log("age: ", age)
+// console.log("status: ",isActive);
+// console.log("salary: ",salary);
 
 //type conversion example
 
 let score = null
 let convertedScore = Number(score);
-console.log(typeof convertedScore);
-console.log(convertedScore);
+// console.log(typeof convertedScore);
+// console.log(convertedScore);
 
 let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(name);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let firstNumber = 100;
+console.log("Before Increment",firstNumber);
+let incremenetNumber = ++firstNumber
+console.log("After Increment",firstNumber)
+console.log("After Increment Number",incremenetNumber);
+
+let anotherIncrement = --incremenetNumber
+console.log("After Another Increment",anotherIncrement)
