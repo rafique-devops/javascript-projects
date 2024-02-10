2b5fef7bbeb6f92319d98bbc44ea3c9b46ffafa8
rafique-devops <thedevops91@gmail.com>
2024-02-10 12:05:07 +0530

    data type & conversion

---
+++ back_to_basics.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all js code as newer version
+
+// console.log(3+3);
+// console.log("Rafique");
+
+//data types with their values
+
+let name = "Rafique"
+let age = 31
+let isActive = true
+let salary = 10000000000
+
+console.log("name: ",name)
+console.log("age: ", age)
+console.log("status: ",isActive);
+console.log("salary: ",salary);
+
+//type conversion example
+
+let score = null
+let convertedScore = Number(score);
+console.log(typeof convertedScore);
+console.log(convertedScore);
