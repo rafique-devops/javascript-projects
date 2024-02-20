1c158dd8564aaf51378fbc00b6f4a819d41c086e
rafique-devops <thedevops91@gmail.com>
2024-02-20 15:23:36 +0530

    added execution.js in main branch

---
+++ execution.js
@@ -0,0 +1,34 @@
+console.log(`Lets' Cooks Omlette`);
+
+const getTools = ()=>{
+    console.log('Step 1: Gather Your Tools');
+}
+
+const getIngredients = ()=> {
+    console.log(`Step 2: Get Your Ingredients`);
+}
+
+
+
+const makeMixture = ()=> {
+    console.log(`Step 3: Make the Egg Mixture`);
+}
+
+
+
+
+const cookingTime = ()=> {
+    console.log(`Step 4: Cooking Time`);
+}
+
+
+
+const serveIt = ()=> {
+    console.log(`Serve It`);
+}
+
+getIngredients();
+makeMixture();
+cookingTime();
+serveIt();
+getTools()
